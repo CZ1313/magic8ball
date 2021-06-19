e0892d88a86d5f8dceb84fe1e3f10285deb080fc
Christina <evilelf13@hotmail.com>
2021-06-18 19:36:35 -0700

    Create input field, button, add img folder, started js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+//I don't know what floor means, but replace 
+//x with whatever number and you will get a random 
+//number between 0 and x
+
+//Math.floor(Math.random() * x);
+
+//then use a switch and put answers in the switch
+
+
+const myImages =new Array ("")
+
+function eightBallAnswers() {
+
+    let image = document.getElementById('answers')
+    if()
+
+}
