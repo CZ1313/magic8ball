fac56ce581f7e10c5776b77ac3a502778d7517ea
Christina <evilelf13@hotmail.com>
2021-06-18 23:49:34 -0700

    begining to work on array and function in js

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 
-//I don't know what floor means, but replace 
-//x with whatever number and you will get a random 
-//number between 0 and x
+let myImages = new Array ('img/a1.png', 'img/a2.png', 'img/a3.png', 'img/a4.png',
+                'img/a5.png', 'img/a6.png', 'img/a7.png', 'img/a8.png',
+                'img/a9.png', 'img/a10.png', 'img/a11.png', 'img/a12.png',
+                'img/a13.png', 'img/a14.png', 'img/a15.png', 'img/a16.png',
+                'img/a17.png', 'img/a18.png', 'img/a19.png', 'img/a20.png');
 
-//Math.floor(Math.random() * x);
-
-//then use a switch and put answers in the switch
-
-
-const myImages =new Array ("")
-
+ //document.getElementById(myButton).addEventListener('click', changeImage);          
+                           
 function eightBallAnswers() {
-
-    let image = document.getElementById('answers')
-    if()
+    let randNum = Math.floor(Math.random() * myImages.length);
+    document.getElementById("answers") .innerHTML[randNum];
 
 }
