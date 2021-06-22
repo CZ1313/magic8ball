81da171936a5c63349a75f514327be7649247c71
Christina <evilelf13@hotmail.com>
2021-06-21 21:56:12 -0700

    trying a different function method to randomNum and if/else statement

--- script.js
+++ script.js
@@ -1,23 +1,69 @@
 
+//Create functon for random number
+function generate_random(max_number) {
 
-
-
-
-let myImages  = document.getElementById("mainImage");
-let imageArray = [ "img/a1.png", "img/a2.png", "img/empty.png"];
-
-
-function eightBallAnswers() {
-    let answer = Math.floor(Math.random() * imageArray.length);
-    myImages.src = imageArray[answer];
+    //Create random number from 0 to max_number
+    return Math.round(Math.random()*max_number);
 }
-   
 
+//Select button element and img element
+let button = document.querySelector("button");
+let answers = document.querySelector("#answers"); 
 
+//Button click event
+button.addEventListener("click", function() {
+  
+//Create random number. Max_number is changed to 21 because thats how many images we have.
+let randomNumber = generate_random(21);
+console.log(randomNumber);
 
+//Turn random number into an answer using the key above.
+let answerText = "";
+ if (randomNumber == 0) {
 
+  answerText ="Yes definitely!" //try this later var img1 =new Image()
+ }                              //                    img.src ="";  
+else if (randomNumber == 1) {
 
+answerText = "No, certainly not!"
+}
+
+else if (randomNumber == 2) {
+
+  answerText = "I really don't care!"
+  }
+
+//Display the answer in the 
+answers.innerHTML = answerText;
+
+});
+
+//function shake() {
+
+  //const ball = document.getElementById("ball")
     
+    //ball.classList.add("shake")
+    //setTimeout(function() {ball.classListList.remove("shake");}, 1000)
+    //setTimeout(function() {answers();}, 1000)
+//}
+  
+
+ //function answers() {
+
+   // const getImage = document.getElementById("ball");
+    //const images = new Array[ "img/empty.png", "img/a1.png", "img/a2.png", "img/a3.png"];
+    //let pickImage = image [Math.floor(Math.random()* image.length)]; 
+
+    //let showAnswer = document.getElementById("answers")
+    //let message = document.createElement("div")
+    //message.setAttribute('id',"message")
+ // }
+
+
+
+    //let answer = Math.floor(Math.random() * imageArray.length);
+    //myImages.src = imageArray[answer];
+ 
 
 
    // myImages.setAttribute("src", imageArray[imageIndex]);
